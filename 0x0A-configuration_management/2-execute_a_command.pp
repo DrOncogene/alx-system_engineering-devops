601429bd0577006c8130b5339fea3e3847449403
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-17 01:56:17 +0100

    kills a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,4 @@
 # kill a command called killmenow
 exec { 'killmenow':
-  path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'pkill killmenow',
-  onlyif  => "test 'pgrep killmenow | wc -l' -gt 0"
+  command => '/usr/bin/pkill killmenow',
 }
