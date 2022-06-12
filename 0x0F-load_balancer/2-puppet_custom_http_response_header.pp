0890e7024ac3b9772de2769a826941771c3fe215
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-06-12 05:48:32 +0100

    seriously?

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,40 +1,9 @@
 # configure a custom http response header
 
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update',
-}
-
-exec { 'install nginx':
-  command => '/usr/bin/apt-get -y install nginx',
-  require => Exec['apt-get update'],
-}
-
-exec { 'allow traffic':
-  command => "/usr/sbin/ufw allow 'Nginx HTTP'",
-  require => Exec['install nginx'],
-}
-
-exec { 'enable firewall':
-  command => '/usr/sbin/ufw enable',
-  require => Exec['allow traffic'],
-}
-
-exec { 'start nginx':
-  command => '/usr/sbin/service nginx start',
-  require => Exec['install nginx'],
-}
-
-file { 'nginx config file':
-  ensure  => 'file',
-  path    => '/etc/nginx/sites-available/default',
-  require => Exec['install nginx'],
-}
-
-exec { 'custom response header':
-  command => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
-  require => File['nginx config file'],
-}
-
-exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
