5ead579a14a20761c6ba8eea9383ae1fef5bcef2
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-06-12 05:44:33 +0100

    changed the sed command

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -32,7 +32,7 @@ file { 'nginx config file':
 }
 
 exec { 'custom response header':
-  command => 'sudo sed -i -r "s|^(\s+)(location / \{)|\1\2\n\1\1add_header X-Served-By $HOSTNAME always\;\n|g" /etc/nginx/sites-available/default',
+  command => 'sudo sed -i -r "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
   require => File['nginx config file'],
 }
 
