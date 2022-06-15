101bf7401cd3eea429973bc9a5c9b2ae617c66ad
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-06-15 18:41:19 +0100

    prints todo datails for given user from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,21 @@ import requests
 import sys
 
 
-def todo_info(user_id: int):
+def todo_info(user_id):
     """prints todo info"""
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     user = requests.get(user_url).json()
     todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
     todos = requests.get(todo_url).json()
-    completed_todos = [todo for todo in todos if todo.get("completed")]
-    print("Employee {} is done with tasks({}/{})".format(
-        user.get("name"),
-        len(completed_todos),
+    completed = [todo for todo in todos if todo["completed"]]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user["name"],
+        len(completed),
         len(todos)
     ))
-    for todo in completed_todos:
-        print("\t {:s}".format(todo.get("title")))
+    for todo in completed:
+        print("\t {}".format(todo["title"]))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("No employee ID passed")
-        sys.exit(1)
     todo_info(sys.argv[1])
